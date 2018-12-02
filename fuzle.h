c7d31d60c65067b6003d148944a5163e09e7166b
David Ruble <david.ruble1@yahoo.com>
2018-12-02 03:06:53 -0800

    Converted to static class, usable with input stream

--- fuzle.h
+++ fuzle.h
@@ -1,9 +1,6 @@
 #pragma once
 
-// TODO: Either break out of readShort/Long chains on error or only show "not enough space" error once
-// TODO: Read from input stream as well as file path
-// TODO: Better comments
-// TODO: Change to static class function (make helpers private)
+// TODO: Either break out of ReadShort/Long chains on error or only show "not enough space" error once
 // TODO: Only store data that we actually need (but comment out rest so know what's there and how many bytes)
 
 #include <string>
@@ -14,57 +11,49 @@
 #include <iomanip>
 #include <iostream>
 
-namespace fuzle {
 
+/* 
+ * Contains all of the data from the xWMA (RIFF) header, up until the "data"
+ * structure that contains the actual audio data.
+ */
+struct XwmaHeader {
+	unsigned long chunkSize;
+	unsigned char XWMA[4];			// "XWMA"
+	unsigned char subchunk1Id[4];	// "fmt "
+	unsigned long subchunk1Size;	// size of fmt chunk
+	unsigned short format;
+	unsigned short numChannels;
+	unsigned long samplesPerSec;
+	unsigned long bytesPerSec;
+	unsigned short blockAlign;
+	unsigned short bitsPerSample;
+	unsigned short extSize;			// Should be 0
+	unsigned char subchunk2Id[4];	// "dpds"
+	unsigned long subchunk2Size;	// size of dpds chunk
+	unsigned long* subchunk2Data;
+};
+
+
+/* 
+ * Static class used to get the length in seconds of a xWMA audio file embedded
+ * in a Skyrim FUZ file.
+ * 
+ * Technically, this works on both FUZ files and xWMA files, but it's meant for
+ * FUZ files.
+ */
+class Fuzle {
+
+public:
 	// Return -1 on failure
-	const float ERROR = -1.0f;
+	static constexpr float ERROR = -1.0f;
 
-	struct WxmaHeader {
-		unsigned long chunkSize;
-		unsigned char XWMA[4];			// "XWMA"
-		unsigned char subchunk1Id[4];	// "fmt "
-		unsigned long subchunk1Size;	// size of fmt chunk
-		unsigned short format;
-		unsigned short numChannels;
-		unsigned long samplesPerSec;
-		unsigned long bytesPerSec;
-		unsigned short blockAlign;
-		unsigned short bitsPerSample;
-		unsigned short extSize;			// Should be 0
-		unsigned char subchunk2Id[4];	// "dpds"
-		unsigned long subchunk2Size;	// size of dpds chunk
-		unsigned long* subchunk2Data;
-	};
-
-	// NOTE: Assumes Little Endian
-	unsigned short readShort(char* buffer, int& startIndex, int bufLen) {
-		if (startIndex + 1 >= bufLen) {
-			std::cerr << "Not enough space in buffer for short!" << std::endl;
-			return -1;
-		}
-
-		unsigned char rightBits = buffer[startIndex++];
-		unsigned char leftBits = buffer[startIndex++];
-		return rightBits | (leftBits << 8);
-	}
-
-	// NOTE: Assumes Little Endian
-	unsigned long readLong(char* buffer, int& startIndex, int bufLen) {
-		if (startIndex + 3 >= bufLen) {
-			std::cerr << "Not enough space in buffer for long!" << std::endl;
-			return -1;
-		}
-
-		// Note: startIndex is modified by each call to readShort()
-		unsigned short rightBits = readShort(buffer, startIndex, bufLen);
-		unsigned short leftBits = readShort(buffer, startIndex, bufLen);
-		return rightBits | (leftBits << 16);
-	}
-
-	// Main entry point
-	float GetAudioLengthInSeconds(const char* filepath) {
-		float audioLength = 0.0f;
-
+	/* 
+	 * Gets the length of the xWMA audio in seconds. String path version.
+	 *
+	 * @param filepath: Path to FUZ file
+	 * @return: Duration in seconds, or -1.0 on error
+	 */
+	static float GetAudioLengthInSeconds(const char* filepath) {
 		// Open the file
 		std::ifstream fs(filepath, std::ifstream::in | std::ifstream::binary);
 		if (!fs) {
@@ -72,24 +61,49 @@ namespace fuzle {
 			return ERROR;
 		}
 
-		// Get the length of the file
+		float len = ComputeLength(fs);
+		fs.close();
+		return len;
+	}
+
+	/* 
+	 * Gets the length of the xWMA audio in seconds. Input stream version.
+	 *
+	 * @param is: Input stream with FUZ data
+	 * @return: Duration in seconds, or -1.0 on error
+	 */
+	static float GetAudioLengthInSeconds(std::istream& is) {
+		return ComputeLength(is);
+	}
+
+private:
+	/*
+	 * Does the heavy lifting for GetAudioLengthInSeconds()
+	 *
+	 * @param is: Input stream with FUZ data
+	 * @return: Duration in seconds, or -1.0 on error
+	*/
+	static float ComputeLength(std::istream& is) {
+		float audioLength = 0.0f;
+
+		// Get the length of the file.
 		//
-		// NOTE: Since the FUZ files are relatively small, we'll read the entire file into memory
-		fs.seekg(0, fs.end);
-		int length = fs.tellg();
-		fs.seekg(0, fs.beg);
+		// NOTE: Since the FUZ files are relatively small, we'll read the entire
+		// file into memory
+		is.seekg(0, is.end);
+		int length = is.tellg();
+		is.seekg(0, is.beg);
 
 		// Read the file into a buffer
 		char* buffer = new char[length];
-		fs.read(buffer, length);
+		is.read(buffer, length);
 
-		if (!fs) {
-			std::cerr << "Could not read entire file. Only " << fs.gcount()
+		if (!is) {
+			std::cerr << "Could not read entire file. Only " << is.gcount()
 				<< " out of " << length << " could be read" << std::endl;
 			delete[] buffer;
 			return ERROR;
 		}
-		fs.close();
 
 		// Find the start index of "RIFF" in the file. This is the start of the
 		// actual xWMA file data.
@@ -101,17 +115,17 @@ namespace fuzle {
 			}
 		}
 
-		// RIFF not found in the file: most likely not a fuz file
+		// RIFF not found in the file: most likely not a FUZ file
 		if (startIndex == -1) {
 			std::cerr << "Unexpected file format" << std::endl;
 			delete[] buffer;
 			return ERROR;
 		}
 
-		// Read in the beginning of the WXMA header (up to WXMA)
-		WxmaHeader hdr = {};
+		// Read in the beginning of the xWMA header (up to XWMA)
+		XwmaHeader hdr = {};
 		int bufInd = startIndex;
-		hdr.chunkSize = readLong(buffer, bufInd, length);
+		hdr.chunkSize = ReadLong(buffer, bufInd, length);
 
 		// Early exit if bad file type
 		if (std::strncmp(buffer + bufInd, "XWMA", 4) != 0) {
@@ -123,14 +137,14 @@ namespace fuzle {
 		// Read in the rest of the header
 		memcpy(hdr.XWMA, buffer + bufInd, 4); bufInd += 4;
 		memcpy(hdr.subchunk1Id, buffer + bufInd, 4); bufInd += 4;
-		hdr.subchunk1Size = readLong(buffer, bufInd, length); 
-		hdr.format = readShort(buffer, bufInd, length);
-		hdr.numChannels = readShort(buffer, bufInd, length);
-		hdr.samplesPerSec = readLong(buffer, bufInd, length);
-		hdr.bytesPerSec = readLong(buffer, bufInd, length);
-		hdr.blockAlign = readShort(buffer, bufInd, length);
-		hdr.bitsPerSample = readShort(buffer, bufInd, length);
-		hdr.extSize = readShort(buffer, bufInd, length);
+		hdr.subchunk1Size = ReadLong(buffer, bufInd, length); 
+		hdr.format = ReadShort(buffer, bufInd, length);
+		hdr.numChannels = ReadShort(buffer, bufInd, length);
+		hdr.samplesPerSec = ReadLong(buffer, bufInd, length);
+		hdr.bytesPerSec = ReadLong(buffer, bufInd, length);
+		hdr.blockAlign = ReadShort(buffer, bufInd, length);
+		hdr.bitsPerSample = ReadShort(buffer, bufInd, length);
+		hdr.extSize = ReadShort(buffer, bufInd, length);
 		
 		// Break out early if no dpds data
 		if (std::strncmp(buffer + bufInd, "dpds", 4) != 0) {
@@ -141,10 +155,10 @@ namespace fuzle {
 
 		// Read in the dpds data
 		memcpy(hdr.subchunk2Id, buffer + bufInd, 4); bufInd += 4;
-		hdr.subchunk2Size = readLong(buffer, bufInd, length) / 4;
+		hdr.subchunk2Size = ReadLong(buffer, bufInd, length) / 4;
 		hdr.subchunk2Data = new unsigned long[hdr.subchunk2Size];
 		for (unsigned int i = 0; i < hdr.subchunk2Size; i++) {
-			hdr.subchunk2Data[i] = readLong(buffer, bufInd, length);
+			hdr.subchunk2Data[i] = ReadLong(buffer, bufInd, length);
 		}
 
 		// Calculate the duration in seconds
@@ -156,4 +170,49 @@ namespace fuzle {
 		delete[] buffer;
 		return audioLength;
 	}
-}
+
+	/* 
+	 * Reads 2 bytes from the buffer.
+	 *
+	 * NOTE: Assumes Little Endian.
+	 *
+	 * @param buffer: Buffer to read from
+	 * @param startIndex: Index to start in the buffer. THIS WILL BE INCREMENTED
+	 *					  BY 2 AS A RESULT OF THIS FUNCTION.
+	 * @param bufLen: Length of the buffer as a whole
+	 * @return: unsigned short representation of the 2 bytes
+	 */
+	static unsigned short ReadShort(char* buffer, int& startIndex, int bufLen) {
+		if (startIndex + 1 >= bufLen) {
+			std::cerr << "Not enough space in buffer for short!" << std::endl;
+			return -1;
+		}
+
+		unsigned char rightBits = buffer[startIndex++];
+		unsigned char leftBits = buffer[startIndex++];
+		return rightBits | (leftBits << 8);
+	}
+
+	/* 
+	 * Reads 4 bytes from the buffer.
+	 *
+	 * NOTE: Assumes Little Endian.
+	 *
+	 * @param buffer: Buffer to read from
+	 * @param startIndex: Index to start in the buffer. THIS WILL BE INCREMENTED
+	 *					  BY 4 AS A RESULT OF THIS FUNCTION.
+	 * @param bufLen: Length of the buffer as a whole
+	 * @return: unsigned long representation of the 4 bytes
+	 */
+	static unsigned long ReadLong(char* buffer, int& startIndex, int bufLen) {
+		if (startIndex + 3 >= bufLen) {
+			std::cerr << "Not enough space in buffer for long!" << std::endl;
+			return -1;
+		}
+
+		// Note: startIndex is modified by each call to ReadShort()
+		unsigned short rightBits = ReadShort(buffer, startIndex, bufLen);
+		unsigned short leftBits = ReadShort(buffer, startIndex, bufLen);
+		return rightBits | (leftBits << 16);
+	}
+};
--- test/main.cpp
+++ test/main.cpp
@@ -10,9 +10,15 @@ const std::string DATA_DIR = "data/";
 // Allow a bit of wiggle room around the expected values (since working with floats)
 const float acceptance = 0.05f;
 
+/* 
+ * Runs fuzle on the specified file.
+ *
+ * @param filename: Name to FUZ file (NOT path)
+ * @param expectedVal: The expected duration in seconds
+ */
 void testFile(std::string filename, float expectedVal) {
 	std::string path = DATA_DIR + filename;
-	float ret = fuzle::GetAudioLengthInSeconds(path.c_str());
+	float ret = Fuzle::GetAudioLengthInSeconds(path.c_str());
 	bool success = (ret >= expectedVal - acceptance && ret <= expectedVal + acceptance);
 	std::cout << "Test " << filename << ": " << "Result: " << ret << "   Success? " << success << std::endl;
 }
@@ -24,6 +30,7 @@ int main() {
 	testFile("mq00__000e0ca8_1.fuz", 3.11f);
 	testFile("mq201__00039f0e_1.fuz", 7.84f);
 		
+	std::cout << "Hit Enter to exit... ";
 	getchar();
 	return 0;
 }
